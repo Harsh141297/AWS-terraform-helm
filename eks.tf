f22e94db6de376266a3f288ec91553ad685962f3
Harshit Choudhary <harshitchoudhary@IT002218.local>
2023-02-20 14:38:35 +0530

    Updated values

--- eks.tf
+++ eks.tf
@@ -2,10 +2,6 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 19.0"
 
-#   default_node_group = {
-#         source_security_group_ids = [aws_security_group.remote_access.id]
-#     }
-
   cluster_name    = local.name
   cluster_version = "1.24"
 
@@ -76,29 +72,6 @@ module "eks" {
       groups   = ["system:masters"]
     },
   ]
-}
-
-
-resource "aws_security_group" "remote_access" {
-  name_prefix = "${local.name}-remote-access"
-  description = "Allow remote SSH access"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    description = "SSH access"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/8"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
 }
 
 
